cf93b0bd63ee5d5a1e7cdda502b1e1e22fdd9b7f
ALEWIS <20676976+angus-lewis@users.noreply.github.com>
2025-04-18 21:11:10 +0930

    Clean up repo, remove figs and data (#29)
    Co-authored-by: angus-lewis <angus.lewis@adelaide.edu.au>

--- examples/forecasting/src/proposal.jl
+++
@@ -1,6 +0,0 @@
-function makeforecastingproposal(config)
-    propconfig = config["inference"]["proposal_parameters"]
-    mu = propconfig["mean"]
-    sigma = reshape(propconfig["cov"], length(mu), length(mu))
-    return MetropolisHastings.MutableMvNormal(mu, sigma)
-end
